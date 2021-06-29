51beb6ce3d4560889c1f21f1713ce0f5176b8588
odin-delrio <odin.delriopineiro@n26.com>
2021-06-29 12:12:11 +0200

    Produce with key.

--- src/main/kotlin/de/n26/demokafkaui/Producer.kt
+++ src/main/kotlin/de/n26/demokafkaui/Producer.kt
@@ -1,35 +1,43 @@
 package de.n26.demokafkaui
 
 import com.google.protobuf.Timestamp
+import org.springframework.kafka.support.KafkaHeaders.MESSAGE_KEY
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.MessageBuilder
 import org.springframework.scheduling.annotation.Scheduled
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Sinks
 import reactor.core.publisher.Sinks.EmitFailureHandler.FAIL_FAST
 import reactor.core.publisher.Sinks.Many
 import java.time.Instant
-import java.util.*
+import java.util.UUID
 import java.util.function.Supplier
 import kotlin.random.Random.Default.nextInt
 
-class Producer : Supplier<Flux<ByteArray>> {
+class Producer : Supplier<Flux<Message<ByteArray>>> {
 
-    private val sink: Many<ByteArray> = Sinks.many().unicast().onBackpressureBuffer()
+    private val sink: Many<Message<ByteArray>> = Sinks.many().unicast().onBackpressureBuffer()
 
     @Scheduled(fixedDelay = 1000, initialDelay = 5000)
     fun sendHello() {
-        val message = Transactions.userTransaction
+        val userTransactionProto = Transactions.userTransaction
             .newBuilder()
             .setUserId(UUID.randomUUID().toString())
             .setAmount(nextInt(-100, 100))
             .setAt(Instant.now().toProtoTimestamp())
             .build()
 
+        val springMessage: Message<ByteArray> = MessageBuilder
+            .withPayload(userTransactionProto.toByteArray())
+            .setHeader(MESSAGE_KEY, userTransactionProto.userId.toByteArray())
+            .build()
+
         synchronized(sink) {
-            sink.emitNext(message.toByteArray(), FAIL_FAST)
+            sink.emitNext(springMessage, FAIL_FAST)
         }
     }
 
-    override fun get(): Flux<ByteArray> = sink.asFlux()
+    override fun get(): Flux<Message<ByteArray>> = sink.asFlux()
 
     private fun Instant.toProtoTimestamp() = Timestamp
         .newBuilder()
