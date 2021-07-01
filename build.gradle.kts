751f05506d8a2766f97c75c600b15fd330955af9
Daniel Dios <daniel.dios@n26.com>
2021-07-01 15:54:58 +0200

    downgrade spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.google.protobuf.gradle.protoc
 
 plugins {
     idea
-    id("org.springframework.boot") version "2.6.0-SNAPSHOT"
+    id("org.springframework.boot") version "2.5.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.10"
     kotlin("plugin.spring") version "1.5.10"
@@ -16,33 +16,23 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://repo.spring.io/milestone") }
-    maven { url = uri("https://repo.spring.io/snapshot") }
-}
-
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2020.0.3")
-    }
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // cloud
-    implementation("org.springframework.cloud:spring-cloud-stream")
-    implementation("org.springframework.cloud:spring-cloud-starter-stream-kafka")
-
     // spring boot
+    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
+    // kafka
+    implementation("org.springframework.kafka:spring-kafka:2.7.3")
+
     // proto
     implementation("com.google.protobuf:protobuf-java:3.6.1")
-
 
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
